cbdd7e70ddc16f203a7f723c9645e6a9a78738fc
0re5ama <shahbibek10@gmail.com>
2018-05-30 19:56:05 +0545

    minor changes

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express'),
-	MongoClient = require('mongodb').MongoClient,
-	bodyParser = require('body-parser'),
-	app = express(),
-	port = 80,
-	config = require('./config');
+    MongoClient = require('mongodb').MongoClient,
+    bodyParser = require('body-parser'),
+    app = express(),
+    config = require('./config');
+    port = config.port,
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoClient.connect(config.db.url, { useNewUrlParser: true }, (err, database) => {
-	if (err)
-		return console.log(err);
-	db = database.db('oshodb');
-	const routes = require('./app/routes') (app, db);
+    if (err)
+        return console.log(err);
+    db = database.db('oshodb');
+    const routes = require('./app/routes') (app, db);
 });
 
 
 app.listen(port, () => {
-	console.log('hello world');
+    console.log('started');
 });
